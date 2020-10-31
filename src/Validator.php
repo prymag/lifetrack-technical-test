7af927e0df5b821438537ebafbf0f3a20fcf5075
perry <pmgd2nd@gmail.com>
2020-10-31 13:45:24 +0800

    Validator

--- src/Validator.php
+++ src/Validator.php
@@ -8,7 +8,7 @@ class Validator
 
     public function isValid(): bool
     {
-        return empty($errors);
+        return empty($this->errors);
     }
 
     public function getErrors(): array
@@ -16,21 +16,35 @@ class Validator
         return $this->errors;
     }
 
-    public function validate(int $studies_per_day, float $study_growth, int $months_forecast): void
+    public function validate(array $request_inputs): void
     {
-        if (empty($studies_per_day) || empty($study_growth) || empty($months_forecast)) {
+        if (!is_array($request_inputs)) {
+            $this->errors[] = 'Invalid Request inputs';
+        }
+
+        $expected_keys = ['studies_per_day', 'study_growth', 'months_forecast'];
+        $keys = array_keys($request_inputs);
+
+        $missing = array_diff($expected_keys, $keys);
+
+        if (count($missing)) {
+            $this->errors[] = 'Missing inputs';
+            return;
+        }
+
+        if ($request_inputs['studies_per_day'] == '' || $request_inputs['months_forecast'] == '') {
             $this->errors[] = 'Please fill up all the fields';
         }
 
-        if (!is_int($studies_per_day)) {
+        if (!is_int($request_inputs['studies_per_day'])) {
             $this->errors[] = 'Invalid studies per day value';
         }
 
-        if (!is_float($study_growth)) {
+        if ($request_inputs['study_growth'] != '' && !is_numeric($request_inputs['study_growth'])) {
             $this->errors[] = 'Invalid study growth value';
         }
 
-        if (!is_int($months_forecast)) {
+        if (!is_int($request_inputs['months_forecast'])) {
             $this->errors[] = 'Invalid month forecast';
         }
     }
