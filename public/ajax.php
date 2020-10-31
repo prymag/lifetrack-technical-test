e62eb160a43c3786cf6d98e3c024960034954e2c
perry <pmgd2nd@gmail.com>
2020-10-31 16:34:07 +0800

    Check if ajax

--- public/ajax.php
+++ public/ajax.php
@@ -2,7 +2,7 @@
 
 require_once('../vendor/autoload.php');
 
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) && empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
     exit;
 }
 
