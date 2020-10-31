433a326672a57df1fe9bb1a36eb4285700f091b2
perry <pmgd2nd@gmail.com>
2020-10-31 13:46:00 +0800

    ajax

--- public/ajax.php
+++ public/ajax.php
@@ -3,9 +3,40 @@
 require_once('../vendor/autoload.php');
 
 use Prymag\Lifetrack\Forecaster;
+use Prymag\Lifetrack\Validator;
 
-$forecaster = new Forecaster();
+try {
+    $data = json_decode(file_get_contents('php://input'), true);
 
-$data = json_decode(file_get_contents('php://input'));
-echo json_encode($data);
-//echo json_encode($forecaster->run());
+    /* $data = [
+        'studies_per_day' => 10000,
+        'study_growth' => 1,
+        'months_forecast' => 3
+    ]; */
+    $validator = new Validator();
+
+    $validator->validate($data);
+
+    if (!$validator->isValid()) {
+        echo json_encode([
+            'success' => false,
+            'error_type' => 'validation',
+            'errors' => $validator->getErrors()
+        ]);
+        exit;
+    }
+
+    $forecaster = new Forecaster();
+    $result = [
+        'success' => true,
+        'data' => $forecaster->run($data['studies_per_day'], $data['months_forecast'], $data['study_growth'])->toArray()
+    ];
+    echo json_encode($result);
+} catch (\Exception $e) {
+    $result = [
+        'success' => false,
+        'error_type' => 'exception',
+        'error' => 'An error occured'
+    ];
+    // Log
+}
