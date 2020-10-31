72d3211695e15ef9ac3de8350252bd5c8fb1b948
perry <pmgd2nd@gmail.com>
2020-10-31 17:42:56 +0800

    fix trailing slash

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -59,7 +59,7 @@ onReady(function() {
 
         if (isFormValid === true) {
             
-            const base_tag = document.getElementById('base_tag').href;
+            let base_tag = document.getElementById('base_tag').href;
             base_tag = base_tag.replace(/(?:\/+(\?))/, '$1').replace(/\/+$/, '') // Remove possible trailing slashes
             const ajax_url = base_tag != '' ? base_tag + '/ajax.php' : 'ajax.php';
             ajaxConnect('POST', ajax_url, getData(), function(el) {
